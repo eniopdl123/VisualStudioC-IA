99f7e76ad7a721d1be3c003a6cb65db8a045cb29
eniopdl123 <eniopdl123@gmail.com>
2021-06-28 12:32:15 -0300

    primeiro commmit

---
+++ Projeto/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace Projeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtQuantidadeDeReestudos = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.txtW3 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtW2 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtW1 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtW0 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtFosforo = new System.Windows.Forms.TextBox();
+            this.txtAcidez = new System.Windows.Forms.TextBox();
+            this.txtCalcio = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnResponder = new System.Windows.Forms.Button();
+            this.btnAprender = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtQuantidadeDeReestudos
+            // 
+            this.txtQuantidadeDeReestudos.Enabled = false;
+            this.txtQuantidadeDeReestudos.Location = new System.Drawing.Point(101, 59);
+            this.txtQuantidadeDeReestudos.Name = "txtQuantidadeDeReestudos";
+            this.txtQuantidadeDeReestudos.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantidadeDeReestudos.TabIndex = 26;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(14, 62);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(86, 17);
+            this.label15.TabIndex = 27;
+            this.label15.Text = "Qtd Estudos";
+            // 
+            // txtW3
+            // 
+            this.txtW3.Enabled = false;
+            this.txtW3.Location = new System.Drawing.Point(101, 205);
+            this.txtW3.Name = "txtW3";
+            this.txtW3.Size = new System.Drawing.Size(100, 20);
+            this.txtW3.TabIndex = 23;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(71, 209);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(29, 17);
+            this.label14.TabIndex = 22;
+            this.label14.Text = "W3";
+            // 
+            // txtW2
+            // 
+            this.txtW2.Enabled = false;
+            this.txtW2.Location = new System.Drawing.Point(101, 171);
+            this.txtW2.Name = "txtW2";
+            this.txtW2.Size = new System.Drawing.Size(100, 20);
+            this.txtW2.TabIndex = 21;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(71, 174);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(29, 17);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "W2";
+            // 
+            // txtW1
+            // 
+            this.txtW1.Enabled = false;
+            this.txtW1.Location = new System.Drawing.Point(101, 133);
+            this.txtW1.Name = "txtW1";
+            this.txtW1.Size = new System.Drawing.Size(100, 20);
+            this.txtW1.TabIndex = 19;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(71, 135);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(29, 17);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "W1";
+            // 
+            // txtW0
+            // 
+            this.txtW0.Enabled = false;
+            this.txtW0.Location = new System.Drawing.Point(101, 96);
+            this.txtW0.Name = "txtW0";
+            this.txtW0.Size = new System.Drawing.Size(100, 20);
+            this.txtW0.TabIndex = 17;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(71, 100);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 17);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "W0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Olive;
+            this.groupBox1.Controls.Add(this.txtFosforo);
+            this.groupBox1.Controls.Add(this.txtAcidez);
+            this.groupBox1.Controls.Add(this.txtCalcio);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(27, 53);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 243);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Entradas";
+            // 
+            // txtFosforo
+            // 
+            this.txtFosforo.Location = new System.Drawing.Point(74, 49);
+            this.txtFosforo.Name = "txtFosforo";
+            this.txtFosforo.Size = new System.Drawing.Size(100, 20);
+            this.txtFosforo.TabIndex = 0;
+            // 
+            // txtAcidez
+            // 
+            this.txtAcidez.Location = new System.Drawing.Point(73, 91);
+            this.txtAcidez.Name = "txtAcidez";
+            this.txtAcidez.Size = new System.Drawing.Size(100, 20);
+            this.txtAcidez.TabIndex = 1;
+            // 
+            // txtCalcio
+            // 
+            this.txtCalcio.Location = new System.Drawing.Point(72, 135);
+            this.txtCalcio.Name = "txtCalcio";
+            this.txtCalcio.Size = new System.Drawing.Size(100, 20);
+            this.txtCalcio.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(20, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Cálcio";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(20, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Acidez";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Fósforo";
+            // 
+            // btnResponder
+            // 
+            this.btnResponder.Location = new System.Drawing.Point(329, 334);
+            this.btnResponder.Name = "btnResponder";
+            this.btnResponder.Size = new System.Drawing.Size(101, 39);
+            this.btnResponder.TabIndex = 17;
+            this.btnResponder.Text = "Responder";
+            this.btnResponder.UseVisualStyleBackColor = true;
+            this.btnResponder.Click += new System.EventHandler(this.btnResponder_Click);
+            // 
+            // btnAprender
+            // 
+            this.btnAprender.Location = new System.Drawing.Point(199, 334);
+            this.btnAprender.Name = "btnAprender";
+            this.btnAprender.Size = new System.Drawing.Size(106, 39);
+            this.btnAprender.TabIndex = 16;
+            this.btnAprender.Text = "Aprender";
+            this.btnAprender.UseVisualStyleBackColor = true;
+            this.btnAprender.Click += new System.EventHandler(this.btnAprender_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(70, 334);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(101, 39);
+            this.btnLimpar.TabIndex = 15;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Olive;
+            this.groupBox2.Controls.Add(this.txtQuantidadeDeReestudos);
+            this.groupBox2.Controls.Add(this.label15);
+            this.groupBox2.Controls.Add(this.txtW3);
+            this.groupBox2.Controls.Add(this.label14);
+            this.groupBox2.Controls.Add(this.txtW2);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.txtW1);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.txtW0);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Location = new System.Drawing.Point(254, 53);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(231, 243);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Parametros";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(514, 476);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnResponder);
+            this.Controls.Add(this.btnAprender);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.groupBox2);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Classificador de Laranjas";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtQuantidadeDeReestudos;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox txtW3;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox txtW2;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtW1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtW0;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtFosforo;
+        private System.Windows.Forms.TextBox txtAcidez;
+        private System.Windows.Forms.TextBox txtCalcio;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnResponder;
+        private System.Windows.Forms.Button btnAprender;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Timer timer1;
+
+
+    }
+}
+
---
+++ Projeto/Form1.cs
@@ -0,0 +1,246 @@
+﻿using Projeto.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private static int sinal(double u)
+        {
+
+            return (u >= 0.0) ? 1 : -1;
+        }
+        public double[] recuperaPesos = new double[4];
+        public int epoca = 0;
+        private void btnAprender_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                #region LIMPAR DADOS DE APRESENTACAO
+
+                txtW0.Text = string.Empty;
+                txtW1.Text = string.Empty;
+                txtW2.Text = string.Empty;
+                txtW3.Text = string.Empty;
+               
+
+                #endregion
+
+
+                
+
+                double[][] amostras = new double[][]{
+                new double[]{-1.0,-1.0, -1.0, -1.0},
+                new double[]{0.1, 0.3, 0.6, 0.5},
+                new double[]{0.4, 0.7, 0.9, 0.7},
+                new double[]{0.7, 0.2, 0.8, 0.1}
+        };
+                
+                double[] saidas = new double[] { 1.0, -1.0, -1.0, 1.0 };//1 tagerina; -1 normal
+                double[] pesos = new double[saidas.Length];
+                
+               Random r1 = new Random(DateTime.Now.Millisecond);
+                for (int i = 0; i < pesos.Length; i++)
+                {
+                    pesos[i] = 1.0 / (r1.Next(100) + 1.0);
+                    
+                    
+                }
+                double taxaAprendizado = 0.05;
+                string erro;
+
+                do
+                {
+
+                    erro = "inexiste";
+
+                    for (int k = 0, l= amostras.GetLength(0); k < l; k++)
+                    {
+                        double u = 0.0;
+
+                        for (int i = 0; i < pesos.Length; i++)
+                        {
+                            u += pesos[i] * amostras[i][k];
+                            
+                        }
+
+
+                        int y = sinal(u);
+                        if (y != saidas[k])
+                        {
+
+                            double aprendizado = taxaAprendizado * (saidas[k] - y);
+
+                            for (int i = 0; i < pesos.Length; i++)
+                            {
+                                pesos[i] = pesos[i] + aprendizado * amostras[i][k];
+                                
+                            }
+
+                            erro = "existe";
+
+                        }
+                    }
+                    epoca++;
+
+
+                }
+                
+
+                while (erro.Equals("existe"));
+                
+                recuperaPesos[0] = pesos[0];
+                recuperaPesos[1] = pesos[1];
+                recuperaPesos[2] = pesos[2];
+                recuperaPesos[3] = pesos[3];
+
+            }catch(Exception ex){
+
+               MessageBox.Show("verifique o erro no treino", ex.Message);
+            
+            }
+            
+            txtW0.Text = Convert.ToString(recuperaPesos[0]);
+            txtW1.Text = Convert.ToString(recuperaPesos[1]);
+            txtW2.Text = Convert.ToString(recuperaPesos[2]);
+            txtW3.Text = Convert.ToString(recuperaPesos[3]);
+            txtQuantidadeDeReestudos.Text = Convert.ToString(epoca);
+
+            }
+
+        
+    
+        private void btnResponder_Click(object sender, EventArgs e)
+        {
+
+
+
+            try
+            {
+
+                if (txtFosforo.Text == string.Empty || txtAcidez.Text == string.Empty || txtCalcio.Text == string.Empty)
+                {
+                    MessageBox.Show(" Preencha todos campos");
+                    return;
+                    
+                }
+
+                
+
+                double Fosforo = Convert.ToDouble(txtFosforo.Text, new System.Globalization.CultureInfo(""));
+                double Acidez = Convert.ToDouble(txtAcidez.Text, new System.Globalization.CultureInfo(""));
+                double Calcio = Convert.ToDouble(txtCalcio.Text, new System.Globalization.CultureInfo(""));
+
+                if ((Fosforo == 0.1 || Fosforo == 0.3 || Fosforo == 0.5 || Fosforo == 0.6) & (Acidez == 0.4 || Acidez == 0.7 || Acidez == 0.9))
+                {
+
+
+
+                    double[] amostras = new double[] { -1.0, Fosforo, Acidez, Calcio };
+                    double[] pesos = new double[] { recuperaPesos[0], recuperaPesos[1], recuperaPesos[2], recuperaPesos[3] };
+                    Form2 ft = new Form2();
+                    Form3 f = new Form3();
+                    double u = 0.0;
+                    for (int k = 0; k < amostras.Length; k++)
+                    {
+                        u += pesos[k] * amostras[k];
+                    }
+                    int y = sinal(u);
+                    if (y == -1)
+                    {
+
+                        f.Show();
+                        MessageBox.Show("Os dados informados é Laranja Pera");
+
+                        txtFosforo.Text = string.Empty;
+                        txtAcidez.Text = string.Empty;
+                        txtCalcio.Text = string.Empty;
+                        f.Close();
+                        
+
+                    }
+                    else if (y == 1)
+                    {
+                        ft.Show();
+
+                        MessageBox.Show("Os dados informados é Laranja tangerina");
+                    }
+
+                    txtFosforo.Text = string.Empty;
+                    txtAcidez.Text = string.Empty;
+                    txtCalcio.Text = string.Empty;
+                    
+                    ft.Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("Desculpa mais não sei a resposta");
+                    txtFosforo.Text = string.Empty;
+                    txtAcidez.Text = string.Empty;
+                    txtCalcio.Text = string.Empty;
+
+                    return;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("verifique o erro na resposta", ex.Message);
+
+            }
+
+
+            
+        }
+        private static Bitmap CaptureRegion(Rectangle rect)
+        {
+
+            Bitmap screenshot = new Bitmap(rect.Height, rect.Width, PixelFormat.Format32bppRgb);
+            using (Graphics gr = Graphics.FromImage(screenshot))
+            {
+                gr.CopyFromScreen(rect.Location, Point.Empty, rect.Size);
+            }
+
+            return screenshot;
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtFosforo.Text = string.Empty;
+            txtAcidez.Text = string.Empty;
+            txtCalcio.Text = string.Empty;
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.BackgroundImage = CaptureRegion(this.DesktopBounds);
+        }
+    }
+}
